7a1f7140ba6ed9a6d19bcfecdc1eab61ce2152fc
Phong <trantuanphong.ttt@users.noreply.github.com>
2018-07-18 21:01:39 +0700

    bill

--- src/java/entity/Bill.java
+++ src/java/entity/Bill.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
  *
  * @author ADMIN
  */
-public class Bill  implements Serializable{
+public class Bill implements Serializable{
     private int billID;
     private int serviceID;
     private String providerName;
